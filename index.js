a59d71314c09e23ea6b8482afefe3f9cdc78ae27
Fernando Moreira <nandomoreira.me@gmail.com>
2017-03-21 20:18:29 -0300

    Adding person file

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+fs.readFile(
+  path.resolve(__dirname, 'info.json'), 'utf8',
+  (err, data) => console.log(err || data)
+);
